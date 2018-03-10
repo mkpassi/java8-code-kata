f8a27e6365654a6b6e64a173501917516f635aa6
Mitesh Passi <meeku@Miteshs-MacBook-Pro.local>
2018-03-10 08:17:57 +0530

    exercise 2

--- collection-interfaces/src/test/java/collection/interfaces/Exercise2Test.java
+++ collection-interfaces/src/test/java/collection/interfaces/Exercise2Test.java
@@ -29,7 +29,7 @@ public class Exercise2Test {
         /**
          * Try to get from key "Alice" using {@link Map#getOrDefault}. If the key doesn't exist, use 30 as default.
          */
-        Integer defaultVal = null;
+        Integer defaultVal = map.getOrDefault("Alice", 30);
 
         assertThat(defaultVal, is(30));
     }
@@ -41,9 +41,9 @@ public class Exercise2Test {
         /**
          * Try to put 2 entry with key as "Alice" value as 32, key as "Joe" and value as 32 using {@link Map#putIfAbsent}.
          */
-        // map.
-        // map.
-
+         map.putIfAbsent("Alice", 32);
+         map.putIfAbsent("Joe", 32);
+      
         assertThat(map.get("Alice"), is(32));
         assertThat(map.get("Joe"), is(22));
     }
@@ -56,9 +56,9 @@ public class Exercise2Test {
          * Merge 2 entry to {@link map} with key="Alice" value=32, key="Joe" value=32 using {@link Map#merge}.
          * If the value already exist for the key, remap with sum value.
          */
-        BiFunction<Object, Object, Integer> remappingFunction = null;
-        // map.
-        // map.
+        BiFunction<Integer, Integer, Integer> remappingFunction = (oldVal,newVal) -> oldVal+newVal;
+        map.merge("Alice", 32, remappingFunction);
+        map.merge("Joe",32,remappingFunction);
 
         assertThat(map.get("Alice"), is(32));
         assertThat(map.get("Joe"), is(54));
@@ -71,13 +71,20 @@ public class Exercise2Test {
         /**
          * Try to increment the value for keys "Joe", "Steven" and "Alice" using {@link Map#computeIfPresent}.
          */
-        BiFunction<Object, Object, Integer> remappingFunction = null;
-        // map.
-        // map.
-        // map.
+        BiFunction<String, Integer, Integer> remappingFunction = (key,value) -> {
+			if(!key.equalsIgnoreCase("Patrick")){
+				value++;
+			}
+        	return value;};
+        
+        map.computeIfPresent("Alice", remappingFunction);
+        map.computeIfPresent("Joe", remappingFunction);
+        map.computeIfPresent("Steven", remappingFunction);
+        map.computeIfPresent("Patrick", remappingFunction);
 
         assertThat(map.get("Joe"), is(23));
         assertThat(map.get("Steven"), is(28));
+        assertThat(map.get("Patrick"), is(28));
         assertThat(map, not(hasKey("Alice")));
     }
 }
