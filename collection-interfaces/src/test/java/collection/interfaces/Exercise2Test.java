b6a6c82c0db05016e15fc51dab27cbbcd33a2b4e
Mitesh Kumar Passi <mpassi@sapient.com>
2018-03-15 16:49:20 +0530

    testcases

--- collection-interfaces/src/test/java/collection/interfaces/Exercise2Test.java
+++ collection-interfaces/src/test/java/collection/interfaces/Exercise2Test.java
@@ -1,21 +1,21 @@
 package collection.interfaces;
 
-import common.test.tool.annotation.Easy;
-
-import org.junit.Test;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 
-import static org.hamcrest.Matchers.hasKey;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import common.test.tool.annotation.Easy;
 
 public class Exercise2Test {
 
-    private final Map<String, Integer> map = new HashMap<String, Integer>() {{
+    private static final Map<String, Integer> map = new HashMap<String, Integer>() {{
         put("Joe", 22);
         put("Steven", 27);
         put("Patrick", 28);
@@ -29,7 +29,7 @@ public class Exercise2Test {
         /**
          * Try to get from key "Alice" using {@link Map#getOrDefault}. If the key doesn't exist, use 30 as default.
          */
-        Integer defaultVal = null;
+        Integer defaultVal = map.getOrDefault("Alice", 30);
 
         assertThat(defaultVal, is(30));
     }
@@ -41,11 +41,13 @@ public class Exercise2Test {
         /**
          * Try to put 2 entry with key as "Alice" value as 32, key as "Joe" and value as 32 using {@link Map#putIfAbsent}.
          */
-        // map.
-        // map.
+         map.putIfAbsent("Alice", 32);
+         map.putIfAbsent("Joe", 32);
 
         assertThat(map.get("Alice"), is(32));
         assertThat(map.get("Joe"), is(22));
+        
+        
     }
 
     @Easy @Test
@@ -56,9 +58,10 @@ public class Exercise2Test {
          * Merge 2 entry to {@link map} with key="Alice" value=32, key="Joe" value=32 using {@link Map#merge}.
          * If the value already exist for the key, remap with sum value.
          */
-        BiFunction<Object, Object, Integer> remappingFunction = null;
-        // map.
-        // map.
+        BiFunction<Integer, Integer, Integer> remappingFunction = (oldVal,NewVal) -> oldVal+NewVal;
+         map.merge("Alice", 32, remappingFunction);
+         map.merge("Joe", 32, remappingFunction);
+        // 
 
         assertThat(map.get("Alice"), is(32));
         assertThat(map.get("Joe"), is(54));
@@ -71,10 +74,10 @@ public class Exercise2Test {
         /**
          * Try to increment the value for keys "Joe", "Steven" and "Alice" using {@link Map#computeIfPresent}.
          */
-        BiFunction<Object, Object, Integer> remappingFunction = null;
-        // map.
-        // map.
-        // map.
+        BiFunction<String, Integer, Integer> remappingFunction = (key,oldVal) -> ++oldVal ;
+        map.computeIfPresent("Joe", remappingFunction);
+        map.computeIfPresent("Steven", remappingFunction);
+        map.computeIfPresent("Alice", remappingFunction);
 
         assertThat(map.get("Joe"), is(23));
         assertThat(map.get("Steven"), is(28));
--- date-and-time-api/src/test/java/date/and/time/api/Exercise2Test.java
+++ date-and-time-api/src/test/java/date/and/time/api/Exercise2Test.java
@@ -7,6 +7,10 @@ import org.junit.Test;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.TemporalAmount;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -18,7 +22,7 @@ public class Exercise2Test {
         /**
          * Create a {@link LocalTime} of 23:07 by using {@link LocalTime#of}
          */
-        LocalTime localTime = null;
+        LocalTime localTime = LocalTime.of(23,07);
 
         assertThat(localTime.toString(), is("23:07"));
     }
@@ -28,7 +32,7 @@ public class Exercise2Test {
         /**
          * Create a {@link LocalTime} of 23:07:03.1 by using {@link LocalTime#of}
          */
-        LocalTime localTime = null;
+        LocalTime localTime = LocalTime.of(23, 07, 03, 100000000);
 
         assertThat(localTime.toString(), is("23:07:03.100"));
     }
@@ -38,7 +42,7 @@ public class Exercise2Test {
         /**
          * Create a {@link LocalTime} of 23:07:03.1 from String by using {@link LocalTime#parse}
          */
-        LocalTime localTime = null;
+        LocalTime localTime = LocalTime.parse("23:07:03.1");
 
         assertThat(localTime.toString(), is("23:07:03.100"));
     }
@@ -51,7 +55,7 @@ public class Exercise2Test {
          * Create a {@link LocalTime} from {@link lt} with hour 21
          * by using {@link LocalTime#withHour} or {@link LocalTime#with}
          */
-        LocalTime localTime = null;
+        LocalTime localTime = lt.with(ChronoField.HOUR_OF_DAY,21);
 
         assertThat(localTime.getHour(), is(21));
         assertThat(localTime.getMinute(), is(lt.getMinute()));
@@ -66,7 +70,7 @@ public class Exercise2Test {
          * Create a {@link LocalTime} from {@link lt} with 30 minutes later
          * by using {@link LocalTime#plusMinutes} or {@link LocalTime#plus}
          */
-        LocalTime localTime = null;
+        LocalTime localTime = lt.plusMinutes(30);
 
         assertThat(localTime.getHour(), is(lt.getHour()));
         assertThat(localTime.getMinute(), is(lt.getMinute() + 30));
@@ -81,7 +85,7 @@ public class Exercise2Test {
          * Create a {@link LocalTime} from {@link lt} with 3 hours before
          * by using {@link LocalTime#minusHours} or {@link LocalTime#minus}
          */
-        LocalTime localTime = null;
+        LocalTime localTime = lt.minus(3, ChronoUnit.HOURS);
 
         assertThat(localTime.getHour(), is(lt.getHour() - 3));
         assertThat(localTime.getMinute(), is(lt.getMinute()));
@@ -97,8 +101,8 @@ public class Exercise2Test {
          * Define a {@link Duration} of 3 hours 30 minutes and 20.2 seconds
          * Create a {@link LocalTime} subtracting the duration from {@link lt} by using {@link LocalTime#minus}
          */
-        Duration duration = null;
-        LocalTime localTime = null;
+        Duration duration = Duration.ofSeconds(3 * 3600 + 30 * 60 + 20, 200 * 1000 * 1000);
+        LocalTime localTime = lt.minus(duration);
 
         assertThat(localTime.getHour(), is(19));
         assertThat(localTime.getMinute(), is(37));
@@ -117,7 +121,7 @@ public class Exercise2Test {
          * Check whether {@link lt2} is before {@link lt} or not
          * by using {@link LocalTime#isAfter} or {@link LocalTime#isBefore}
          */
-        boolean isBefore2307 = false;
+        boolean isBefore2307 = lt2.isBefore(lt);
 
         assertThat(isBefore2307, is(true));
     }
@@ -129,7 +133,7 @@ public class Exercise2Test {
         /**
          * Create a {@link LocalTime} from {@link lt} truncated to minutes by using {@link LocalTime#truncatedTo}
          */
-        LocalTime localTime = null;
+        LocalTime localTime = lt.truncatedTo(ChronoUnit.MINUTES);
 
         assertThat(lt.toString(), is("23:07:30.500"));
         assertThat(localTime.toString(), is("23:07"));
